c21cdb86e064489cfc696040c0097a6a4fc2abc3
zediious <zediious@aol.com>
2022-11-20 22:36:08 -0500

    Increase character length count
    This could be infinite, I will implement a limit on announcement counts instead

--- bot/config/settings.py
+++ bot/config/settings.py
@@ -10,5 +10,5 @@ DISCORD_GUILD = 215634815469223937
 # List of Steam API requests, with placeholders for use in functions
 STEAM_API_REQUESTS = {
     "get_all_games": "https://api.steampowered.com/ISteamApps/GetAppList/v2/",
-    "get_news": "http://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/?appid=REPLACE_APP&count=REPLACE_COUNT&maxlength=300&format=json"
+    "get_news": "http://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/?appid=REPLACE_APP&count=REPLACE_COUNT&maxlength=1000000&format=json"
 }
