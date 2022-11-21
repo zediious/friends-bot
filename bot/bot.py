e5c2dc5292811e1d711217fa6becd42c15f43e84
zediious <zediious@aol.com>
2022-11-20 23:34:39 -0500

    Refactor + fix news command
    News command would fail if news contents were too large. It would also fail due to malformed urls from steam API ( some of spaces, just removed spaces from any URL containing them
    - Added command to forcefully refresh app list

--- bot/bot.py
+++ bot/bot.py
@@ -1,7 +1,6 @@
 import discord
 from discord.ext import commands
-from os.path import getsize
-from json import dumps, loads
+from json import loads
 from requests import get
 from re import compile
 from datetime import datetime
@@ -18,21 +17,7 @@ friend_bot = commands.Bot(command_prefix='!', description=SETTINGS.DESCRIPTION, 
 # Define events to listen for
 @friend_bot.event
 async def on_ready():
-    try:
-        with open('steam_app_list.json', "r+") as app_list:
-            if getsize('steam_app_list.json') < 1024:
-                app_json = loads(get(SETTINGS.STEAM_API_REQUESTS["get_all_games"]).text)
-                app_list.write(dumps(app_json, indent=4))
-                app_list.close()
-
-    except:
-        create_file = open('steam_app_list.json', 'w')
-        create_file.close()
-        with open('steam_app_list.json', "r+") as app_list:
-            if getsize('steam_app_list.json') < 1024:
-                app_json = loads(get(SETTINGS.STEAM_API_REQUESTS["get_all_games"]).text)
-                app_list.write(dumps(app_json, indent=4))
-                app_list.close()
+    await UTILITY.get_app_list()
     print(f'Logged in as {friend_bot.user} (ID: {friend_bot.user.id})')
     try:
         synced = await friend_bot.tree.sync()
@@ -53,25 +38,38 @@ async def on_presence_update(before, after):
     pass
 
 # Define Bot commands
-@friend_bot.tree.command(name="gamenews")
+@friend_bot.tree.command(name="refresh_app_list", description="Forcefully refresh the cached Steam App List. Do not do often!")
+@discord.app_commands.describe()
+async def gamenews(interaction: discord.Interaction):
+    """
+    Forcefully refresh the cached Steam App List. Do not do often!
+    """
+    await UTILITY.get_app_list(delete_old=True)
+    await interaction.response.send_message(content="steam_app_list.json has been refreshed. Do not run this command often!")
+
+@friend_bot.tree.command(name="gamenews", description="Gets announcements for a game from Steam News and sends a message with collected news items")
 @discord.app_commands.describe(game = "Type the name of a game (be exact!)", amount= "The amount of Announcements you want to fetch")
 async def gamenews(interaction: discord.Interaction, game: str, amount: str):
     """
     Gets announcements for a game from Steam API and sends an embed with them
     """
-    embed_list = []
-    game_id = await UTILITY.find_game_id(game)
-    if game_id == None:
-        await interaction.response.send_message(content="A game with the name you provided was not found")
-    news_json = loads(get(SETTINGS.STEAM_API_REQUESTS["get_news"].replace('REPLACE_APP', game_id).replace('REPLACE_COUNT', amount)).text)
-    header = discord.Embed(title=f"News for: {game}", url=SETTINGS.STEAM_API_REQUESTS["get_news"].replace('REPLACE_APP', game_id).replace('REPLACE_COUNT', amount))
-    embed_list.append(header)
-    REMOVE_TAGS = compile(r'<[^>]+>')
-    for message in news_json["appnews"]["newsitems"]:
-        message_content = REMOVE_TAGS.sub('', message["contents"])
-        embed_list.append(discord.Embed(title=message["title"], description=f"{message_content}\n\nPosted on `{datetime.fromtimestamp(message['date']).isoformat('#', 'hours')}`", url=message["url"]))
+    if int(amount) <= 10:
+        embed_list = []
+        bad_message_counter = 0
+        game_id = await UTILITY.find_game_id(game)
+        if game_id == None:
+            await interaction.response.send_message(content="A game with the name you provided was not found")
+        news_json = loads(get(SETTINGS.STEAM_API_REQUESTS["get_news"].replace('REPLACE_APP', game_id).replace('REPLACE_COUNT', amount)).text)
+        REMOVE_TAGS = compile(r'<[^>]+>')
+        for message in news_json["appnews"]["newsitems"]:
+            message_content = REMOVE_TAGS.sub('', message["contents"])
+            if len(message_content) > 500:
+                message_content = (message_content[:499] + '..')
+            embed_list.append(discord.Embed(title=message["title"], description=f"{message_content}\n\nPosted on `{datetime.fromtimestamp(message['date']).isoformat('#', 'hours')}`", url=message["url"].replace(' ', '')))
 
-    await interaction.response.send_message(embeds=embed_list)
+        await interaction.response.send_message(content=f"```Last {amount} news items for:``````{game}```" ,embeds=embed_list)
+    else: 
+        await interaction.response.send_message(content=f"The maximum amount of news items that can be retrieved is 10")
 
 # Run the bot
 friend_bot.run(SETTINGS.TOKEN)
--- bot/util/utility.py
+++ bot/util/utility.py
@@ -1,4 +1,30 @@
-from json import load
+from json import load, dumps, loads
+from os.path import getsize
+from requests import get
+
+from config import settings as SETTINGS
+
+async def get_app_list(delete_old=False):
+    """
+    If steam_app_list.json is empty or does not exist, get the data from
+    Steam API and save. If `delete_old` parameter is passes as True, new data
+    will be fetched no matter the current state of steam_app_list.json
+    """
+    try:
+        with open('steam_app_list.json', "r+") as app_list:
+            if getsize('steam_app_list.json') < 1024 or delete_old == True:
+                app_json = loads(get(SETTINGS.STEAM_API_REQUESTS["get_all_games"]).text)
+                app_list.write(dumps(app_json, indent=4))
+                app_list.close()
+
+    except:
+        create_file = open('steam_app_list.json', 'w')
+        create_file.close()
+        with open('steam_app_list.json', "r+") as app_list:
+            if getsize('steam_app_list.json') < 1024 or delete_old == True:
+                app_json = loads(get(SETTINGS.STEAM_API_REQUESTS["get_all_games"]).text)
+                app_list.write(dumps(app_json, indent=4))
+                app_list.close()
 
 async def find_game_id(game_arg):
     """
